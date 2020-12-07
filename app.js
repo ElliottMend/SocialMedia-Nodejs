5ba05de1e259e0665f2d05497460809e6c5bcaa3
Elliottt <elliottmend@gmail.com>
2020-12-07 11:28:06 -0600

    update cors

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ limit: "100mb", extended: true }));
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
