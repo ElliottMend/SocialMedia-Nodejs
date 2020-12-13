84483c1722d0a0eabf6b5fa612ea8ef406092eff
Elliottt <elliottmend@gmail.com>
2020-12-13 15:38:20 -0600

    fixed user profile posts not showing

--- controller/user/getUserProfile.js
+++ controller/user/getUserProfile.js
@@ -2,7 +2,7 @@ const findUsername = require("../findUsername"),
   Post = require("../../models/posts");
 const getUserProfile = async (req, res, next) => {
   const posts = await Post.find({ author: req.params.username, show: true });
-  if (user.length === 0) {
+  if (!req.params.username) {
     res.status(400).send({ message: "This user does not exist" });
   } else {
     res.send(posts);
