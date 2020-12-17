27c0b2b4a2083a0338cffc3600931027de982606
Elliottt <elliottmend@gmail.com>
2020-12-17 13:45:33 -0600

    user edit

--- routes/user.js
+++ routes/user.js
@@ -2,12 +2,14 @@ const router = require("express").Router(),
   userEdit = require("../controller/user/userEdit"),
   getUserProfile = require("../controller/user/getUserProfile"),
   verify = require("../controller/userAuth/verify"),
-  getUserLikes = require("../controller/user/getUserLikes");
+  getUserLikes = require("../controller/user/getUserLikes"),
+  getUserEdit = require("../controller/user/getUserEdit");
 
 router.put("/userEdit", verify, userEdit, async (req, res) => {});
 router.get("/users/:username", verify, getUserProfile, async (req, res) => {});
 router.get("/checkJWT", verify, (req, res) => {
   res.send(res.locals.username);
 });
+router.get("/getUserEdit", verify, getUserEdit, () => {});
 router.get("/getUserLikes/:username", verify, getUserLikes, () => {});
 module.exports = router;
