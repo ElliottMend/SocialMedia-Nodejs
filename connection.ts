9fd7e041ce26114bba03c193f4e159d6aecb4ec7
Elliottt <elliottmend@gmail.com>
2021-04-12 14:10:42 -0500

    b

--- connection.ts
+++ connection.ts
@@ -4,8 +4,8 @@ import { secrets } from "./app";
 const connectionString = `postgres://${secrets.USER}:${secrets.PASSWORD}@${
   secrets.DOCKERBUILD
     ? secrets.NODE_ENV
-      ? `${secrets.TEST_DATABASE_CONTAINER}:${secrets.DATABASE_PORT}`
-      : `${secrets.DATABASE_CONTAINER}:${secrets.DATABASE_PORT}`
+      ? `${secrets.TEST_DATABASE_CONTAINER}:${secrets.DATABASE_PORT}/${secrets.TEST_DATABASE}`
+      : `${secrets.DATABASE_CONTAINER}:${secrets.DATABASE_PORT}/${secrets.DATABASE}`
     : `localhost:5400/${
         secrets.NODE_ENV ? secrets.TEST_DATABASE : secrets.DATABASE
       }`
