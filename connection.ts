3f78d4a637814d6cd313182717d4b3c4d78f22dd
Elliottt <elliottmend@gmail.com>
2021-04-12 13:24:27 -0500

    updated connection string

--- connection.ts
+++ connection.ts
@@ -1,26 +1,18 @@
 import { Pool } from "pg";
 import { secrets } from "./app";
-export let pool: Pool;
-if (secrets.DOCKERBUILD) {
-  pool = new Pool({
-    connectionString: `postgres://${secrets.USER}:${secrets.PASSWORD}@${
-      !secrets.NODE_ENV
-        ? `${secrets.DATABASE_CONTAINER}:${secrets.DATABASE_PORT}`
-        : `${secrets.TEST_DATABASE_CONTAINER}:${secrets.TEST_DATABASE_PORT}`
-    }/${secrets.DATABASE}`,
-    max: 20,
-    idleTimeoutMillis: 30000,
-    connectionTimeoutMillis: 2000,
-  });
-} else {
-  pool = new Pool({
-    connectionString: `postgres://${secrets.USER}:${secrets.PASSWORD}@${
-      !secrets.NODE_ENV
-        ? `localhost:5400/SocialMedia`
-        : `localhost:5400/SocialMediaTest`
-    }`,
-    max: 20,
-    idleTimeoutMillis: 30000,
-    connectionTimeoutMillis: 2000,
-  });
-}
+
+const connectionString = `postgres://${secrets.USER}:${secrets.PASSWORD}@${
+  secrets.DOCKERBUILD
+    ? secrets.NODE_ENV
+      ? secrets.DATABASE_CONTAINER
+      : secrets.TEST_DATABASE_CONTAINER
+    : `localhost:${secrets.DATABASE_PORT}
+    /${secrets.NODE_ENV ? secrets.TEST_DATABASE : secrets.DATABASE}`
+}`;
+
+export const pool = new Pool({
+  connectionString,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+});
